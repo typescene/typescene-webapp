05f03f25a9ebefce04053faa5a835f4fdba923b1
Jelmer Cormont <jcormont@gmail.com>
2018-11-15 22:18:26 +0800

    Update typings

--- typings/HMR.d.ts
+++ typings/HMR.d.ts
@@ -1,14 +1,3 @@
-declare global {
-    const module: {
-        id: string;
-        exports: any;
-        hot: {
-            data: any;
-            accept(callback: () => void): void;
-            dispose(callback: () => void): void;
-        };
-    };
-}
 export declare namespace HMR {
-    function enableViewReload(viewModule: typeof module): void;
+    function enableViewReload(viewModule: any): void;
 }
