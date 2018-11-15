3a3d9bdec6b9b81746361d4693096c230829c30a
Jelmer Cormont <jcormont@gmail.com>
2018-11-15 21:47:35 +0800

    Minor changes for use with Electron

--- src/HMR.ts
+++ src/HMR.ts
@@ -1,22 +1,9 @@
 import { Application, Component } from "typescene";
 
-/** Global commonjs `module` variable */
-declare global {
-    const module: {
-        id: string;
-        exports: any;
-        hot: {
-            data: any;
-            accept(callback: () => void): void;
-            dispose(callback: () => void): void;
-        }
-    };
-}
-
 /** Encapsulates hot module reload (HMR) functions for a view module, which must export exactly one component constructor as `default` or `View` */
 export namespace HMR {
     /** Enable hot module reloading for given view module, which must export exactly one component constructor as `default` or `View` */
-    export function enableViewReload(viewModule: typeof module) {
+    export function enableViewReload(viewModule: any) {
         if (viewModule.hot) {
             // add a callback for the incoming module:
             if (viewModule.hot.data && viewModule.hot.data.updateActivity) {
