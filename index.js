ccc08121f5cb42c7ec53030979d60ef3ec5dfbba
Jelmer Cormont <jcormont@gmail.com>
2019-05-06 21:34:37 +0800

    Change d.ts output location, update package.json

---
+++ index.js
@@ -0,0 +1,6 @@
+/*! *******************************************************
+Typescene framework -- https://typescene.dev
+See LICENSE and README files for usage details
+******************************************************** */
+export * from "./dist";
+
